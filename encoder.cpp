56a8d6e2c913651b678e19b8549f9e18da2934c4
saeid <emadisaeidabad@gmail.com>
2024-05-04 15:26:59 +1030

    log level function for show each encoded bit

--- encoder.cpp
+++ encoder.cpp
@@ -1,7 +1,8 @@
 #include "encoder.h"
 #include <iostream>
+#include <bitset>
 
-encoder :: encoder() {
+encoder::encoder() {
 	/* create new object and initial values : 
 		- state = 00
 	*/
@@ -9,6 +10,15 @@ encoder :: encoder() {
 		"encoder enable"
 		<< std::endl;
 	state = 0;
+}
+
+void encoder::logLevel(__int8 state, bool bit, __int8 decoded) {
+	std::cout <<
+		"[---- bit log ----]" << std::endl <<
+		"  state : " << std::bitset<2>(state) << std::endl <<
+		"  bit : " << std::bitset<1>(bit) << std::endl <<
+		"  decoded : " << std::bitset<1>(decoded) << std::endl <<
+		"[----         ----]" << std::endl;
 }
 
 encoder :: ~encoder() {
