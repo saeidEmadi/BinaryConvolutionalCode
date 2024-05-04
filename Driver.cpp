4e1ead8548abe1a5d1434345ee83a8df710ee6c8
saeid <emadisaeidabad@gmail.com>
2024-05-05 01:54:18 +1030

    test app only for encoder

--- Driver.cpp
+++ Driver.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <bitset>
+#include "encoder.h"
+#include "decoder.h"
 
 int main(){
-    std::cout << "\n [Binary Convolutional Code run ...]";
+    std::cout << "\n [Binary Convolutional Code run ...]\n";
+    std::cout << " Errors and Error Correcting Codes\n";
+    encoder e = encoder();
+    int bit;
+    while (true) {
+        std::cout << "enter bit : ";
+        std::cin >> bit;
+        if (bit == 10) {
+            break;
+        }
+        e.encode(bool(bit & 0x1));
+    }
+
     return 0;
 }
