cf39c7c6ee354578852dacaf7c5ae0168d392853
saeid <emadisaeidabad@gmail.com>
2024-05-05 01:53:41 +1030

    add distance recursive calc

--- decoder.h
+++ decoder.h
@@ -22,15 +22,30 @@ class decoder {
 
 	private:
 		/*
-			private var __int8 state:2
-			2 bit use for show state
-		*/
-		__int8 state : 2;
-		/*
 			private func logLevel()
 			define print pattern for print
 			Machine state, input 2 bit
 			decoded 2bit and nextState
 		*/
 		void logLevel(__int8 state, __int8 recv, __int8 encoded, __int8 nextState);
+		/*
+			calculate the Hamming distance between two integers
+			Parameters:
+				- __int8 x
+				- __int8 y
+			Returns:
+				- __int8 distance
+		*/
+		__int8 hammingDistance(__int8 x, __int8 y);
+		/*
+			calculate the distance for error correction
+			use DFS idea (recursive)
+			Parameters:
+				- int level
+				- __int8 state
+				- __int8 bit
+			Returns:
+				- __int8 distance
+		*/
+		__int8 distanceCal(int level, __int8 state, __int8 bit);
 };
